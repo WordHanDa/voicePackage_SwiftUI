50d51834d902869009e6b62dbc0ea1d17b18cef8
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-06-09 17:23:22 +0800

    Create voicePackage_SwiftUIApp.swift

---
+++ voicePackage_SwiftUI/voicePackage_SwiftUI/voicePackage_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  voicePackage_SwiftUIApp.swift
+//  voicePackage_SwiftUI
+//
+//  Created by macbook pro on 2023/6/9.
+//
+
+import SwiftUI
+
+@main
+struct voicePackage_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
