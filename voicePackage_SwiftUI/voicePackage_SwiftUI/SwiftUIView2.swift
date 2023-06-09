c258fa1145499781ff3b2ef1dce07e67650952ca
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-06-10 00:31:55 +0800

    Create SwiftUIView2.swift

---
+++ voicePackage_SwiftUI/voicePackage_SwiftUI/SwiftUIView2.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView2.swift
+//  voicePackage_SwiftUI
+//
+//  Created by macbook pro on 2023/6/9.
+//
+
+import SwiftUI
+
+struct SwiftUIView2: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView2_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView2()
+    }
+}
