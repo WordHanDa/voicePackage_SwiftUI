34b8b4c0c63acb97722004a44c083eef8d3d1aa6
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-06-09 17:33:38 +0800

    Update ContentView.swift

--- voicePackage_SwiftUI/voicePackage_SwiftUI/ContentView.swift
+++ voicePackage_SwiftUI/voicePackage_SwiftUI/ContentView.swift
@@ -8,14 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+    var body: some View{
+        Text("Hello, World")
     }
 }
 
