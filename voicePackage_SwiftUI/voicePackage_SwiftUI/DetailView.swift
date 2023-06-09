0ffc95e8ec1fa66d3cc13baa2db717685426a033
b1129012 <88619256+WordHanDa@users.noreply.github.com>
2023-06-10 00:31:56 +0800

    Create DetailView.swift

---
+++ voicePackage_SwiftUI/voicePackage_SwiftUI/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  voicePackage_SwiftUI
+//
+//  Created by macbook pro on 2023/6/9.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text("This is DetailView")
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView()
+    }
+}
